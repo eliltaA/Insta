42cf2ec07e7ff8acee4b38568302222fcdc033de
“eliltaA” <eliltatabrham@gmail.com>
2023-08-13 13:09:46 -0700

    user controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::UsersController < ApplicationController
+    def index
+        @users = User.all 
+        render :index
+      end 
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          sign_in(@user)
+          render "api/users/show"
+        else
+          render json: @user.errors.full_messages, status: 422
+        end
+      end
+    
+      def show 
+        @user = User.find(params[:id])
+        # @posts = @user.posts
+        render :show
+      end
+    
+      def update 
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          render :show 
+        else
+          render json: @user.errors.full_messages, status: 422
+        end
+      end
+    
+      private
+      def user_params
+        params.require(:user).permit(:username, :email, :password, :name, :bio)
+      end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,44 +13,37 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    
-    validates :username, :email, :password_digest, :session_token, presence: true
+    has_secure_password
+    validates :username, :email, presence: true
     validates :username, uniqueness: true
     validates :email, uniqueness: true
     validates :password, length: { minimum: 6 }, allow_nil: true
     
     before_validation :ensure_session_token
-    attr_reader :password
-
-  has_many :posts,
-    primary_key: :id,
-    foreign_key: :author_id,
-    class_name: :Post,
-    dependent: :destroy
+   
+#   has_many :posts,
+#     primary_key: :id,
+#     foreign_key: :author_id,
+#     class_name: :Post,
+#     dependent: :destroy
     
-  has_one_attached :profile_photo, dependent: :destroy  
-  has_many :comments 
-  has_many :likes
+#   has_one_attached :profile_photo, dependent: :destroy  
+#   has_many :comments 
+#   has_many :likes
 
 
-  has_many :followers
-  has_many :followees
-  has_many :likes
+#   has_many :followers
+#   has_many :followees
+#   has_many :likes
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
-    return nil unless user
-    user.is_password?(password) ? user : nil
-  end
-
-  def password=(password)
-    @password = password
-    self.password_digest = BCrypt::Password.create(password)
-  end
-
-  def is_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password?(password)
-  end
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.authenticate(password)
+            user
+        else
+            nil
+        end
+    end
 
   def reset_session_token!
     self.session_token = SecureRandom.urlsafe_base64(16)
@@ -58,10 +51,16 @@ class User < ApplicationRecord
     self.session_token
   end
 
-  private
-
   def ensure_session_token
      self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
+
+  private
+  def generate_unique_token
+    loop do
+        token = SecureRandom.urlsafe_base64
+        return token unless User.exists?(session_token: token)
+    end
+end
 
 end
