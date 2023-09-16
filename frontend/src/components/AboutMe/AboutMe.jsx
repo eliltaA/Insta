ec77035e7532bc5da6ebdb9db5f34d49ba006f08
eliltaA <eliltatabrham@gmail.com>
2023-09-15 20:00:20 -0700

    seeds

--- frontend/src/components/AboutMe/AboutMe.jsx
+++ frontend/src/components/AboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './aboutMe.css';
+import './AboutMe.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
